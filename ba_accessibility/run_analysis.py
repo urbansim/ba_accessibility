93f8da76ca5319f0b0ad523a5a25cd3c0b7ae92c
jessicacamacho <jessica.camacho@berkeley.edu>
2022-01-21 15:02:34 -0500

    Adds logic to compare baseline and project scenarios

--- ba_accessibility/run_analysis.py
+++ ba_accessibility/run_analysis.py
@@ -12,16 +12,35 @@ from geopandas import GeoDataFrame
 from shapely.geometry import Point
 from shapely.geometry import LineString
 from urbanaccess.gtfs.gtfsfeeds_dataframe import gtfsfeeds_dfs
+import h3pandas
+
+def process_update_jobs():
+    s_time = time.time()
+    zones = gpd.read_file('data/original/jobs/Empleo.shp')
+    resolution=10
+    hexagons = zones.h3.polyfill_resample(resolution)
+    hexagons = hexagons.to_crs(22192)
+    hexagons['hex_area'] = hexagons.geometry.area
+    zone_area = hexagons.groupby('ID')['hex_area'].sum().reset_index()
+    zone_area = zone_area.rename(columns={'hex_area': 'zone_area'})
+    hexagons = hexagons.merge(zone_area, on='ID', how='left')
+    job_cols = ['jobs', 'job_a', 'job_b', 'job_c', 'job_d', 'job_h']
+    for col in job_cols:
+        hexagons[col] = round(hexagons[col].fillna(0).astype('int') * hexagons['hex_area'] / hexagons['zone_area'])
+    if not os.path.exists('data/processed/jobs'):
+        os.makedirs('./data/processed/jobs')
+    hexagons[['geometry'] + job_cols].to_file('data/processed/jobs/jobs_hexagons.shp')
+    print('Took {:,.2f} seconds to process jobs shapefile'.format(time.time() - s_time))
 
 
-def process_gtfs():
+def process_update_gtfs():
     print('Processing all GTFS feeds to required input format')
     copy_files()
-    for dir in sorted(os.listdir('data/processed_gtfs')):
+    for dir in sorted(os.listdir('data/processed/gtfs_baseline')):
         print('---------------------------')
         print('Formatting', dir)
         s_time = time.time()
-        frequencies = glob.glob('./data/processed_gtfs/%s/frequencies.txt' % dir)
+        frequencies = glob.glob('./data/processed/gtfs_baseline/%s/frequencies.txt' % (dir))
         if len(frequencies) > 0:
             frequencies, stop_times, trips, routes, agencies = read_files(dir)
             frequencies, stop_times = format_inputs(frequencies, stop_times)
@@ -29,21 +48,22 @@ def process_gtfs():
             expanded_trips = expand_trips(trips, unique_trips)
             export_outputs(dir, stop_times, trips, expanded_stop_times, expanded_trips)
         print('Took %s seconds to process %s' %(time.time() - s_time, dir))
+    create_gtfs_with_project()
 
 
 def copy_files():
     for mode in ['subte', 'trenes', 'colectivos']:
-        original_path = ('data/original_gtfs/%s' % mode)
-        processed_path = ('data/processed_gtfs/%s' % mode)
+        original_path = ('data/original/gtfs_baseline/%s' % (mode))
+        processed_path = ('data/processed/gtfs_baseline/%s' % (mode))
         agencies = pd.read_csv('%s/agency.txt' % original_path)
         if len(agencies.index) == 1:
-            if not os.path.exists('data/processed_gtfs/%s' % mode):
-                os.makedirs('./data/processed_gtfs/%s' % mode)
-            gtfs_files = ['agency', 'calendar', 'feed_info', 'routes', 'stop_times', 'stops', 'trips', 'frequencies']  # 'shapes',
+            if not os.path.exists('data/processed/gtfs_baseline/%s' % (mode)):
+                os.makedirs('./data/processed/gtfs_baseline/%s' % (mode))
+            gtfs_files = ['agency', 'calendar', 'feed_info', 'routes', 'stop_times', 'stops', 'trips', 'frequencies']
             for file in gtfs_files:
-                if os.path.isfile('data/original_gtfs/%s/%s.txt' % (mode, file)):
-                    shutil.copy('./data/original_gtfs/%s/%s.txt' % (mode, file),
-                                './data/processed_gtfs/%s/%s.txt' % (mode, file))
+                if os.path.isfile('data/original/gtfs_baseline/%s/%s.txt' % (mode, file)):
+                    shutil.copy('./data/original/gtfs_baseline/%s/%s.txt' % (mode, file),
+                                './data/processed/gtfs_baseline/%s/%s.txt' % (mode, file))
         else:
             feed_info = pd.read_csv('%s/feed_info.txt' % original_path)
             calendar = pd.read_csv('%s/calendar.txt' % original_path)
@@ -74,11 +94,11 @@ def copy_files():
 
 def read_files(mode):
     print('Reading files')
-    frequencies = pd.read_csv('data/processed_gtfs/%s/frequencies.txt' % mode)
-    stop_times = pd.read_csv('data/processed_gtfs/%s/stop_times.txt' % mode)
-    trips = pd.read_csv('data/processed_gtfs/%s/trips.txt' % mode)
-    routes = pd.read_csv('data/processed_gtfs/%s/routes.txt' % mode)
-    agencies = pd.read_csv('data/processed_gtfs/%s/agency.txt' % mode)
+    frequencies = pd.read_csv('data/processed/gtfs_baseline/%s/frequencies.txt' % mode)
+    stop_times = pd.read_csv('data/processed/gtfs_baseline/%s/stop_times.txt' % mode)
+    trips = pd.read_csv('data/processed/gtfs_baseline/%s/trips.txt' % mode)
+    routes = pd.read_csv('data/processed/gtfs_baseline/%s/routes.txt' % mode)
+    agencies = pd.read_csv('data/processed/gtfs_baseline/%s/agency.txt' % mode)
     return frequencies, stop_times, trips, routes, agencies
 
 
@@ -165,30 +185,83 @@ def expand_trips(trips, unique_trips):
     return expanded_trips
 
 
-def export_outputs(mode, stop_times, trips, expanded_stop_times, expanded_trips):
+def export_outputs( mode, stop_times, trips, expanded_stop_times, expanded_trips):
     print('Exporting stop times')
-    expanded_stop_times[stop_times.columns].to_csv('./data/processed_gtfs/%s/stop_times.txt' % mode, index=False)
+    expanded_stop_times[stop_times.columns].to_csv('./data/processed/gtfs_baseline/%s/stop_times.txt' % (mode), index=False)
     print('Exporting trips')
-    expanded_trips[trips.columns].to_csv('./data/processed_gtfs/%s/trips.txt' % mode, index=False)
+    expanded_trips[trips.columns].to_csv('./data/processed/gtfs_baseline/%s/trips.txt' % (mode), index=False)
     print('Output export done')
 
 
+def create_gtfs_with_project():
+    for dir in sorted(os.listdir('data/processed/gtfs_baseline')):
+        if not os.path.exists('data/processed/gtfs_project/%s' % (dir)):
+            os.makedirs('./data/processed/gtfs_project/%s' % (dir))
+        gtfs_files = ['agency', 'calendar', 'feed_info', 'routes', 'stop_times', 'stops', 'trips', 'frequencies']
+        for file in gtfs_files:
+            if os.path.isfile('data/processed/gtfs_baseline/%s/%s.txt' % (dir, file)):
+                shutil.copy('./data/processed/gtfs_baseline/%s/%s.txt' % (dir, file),
+                            './data/processed/gtfs_project/%s/%s.txt' % (dir, file))
+    updates = pd.read_csv('data/original/project_modifications.csv')
+    updates = updates.fillna(0)
+    updates['stop_id'] = updates['stop_id'].astype('int')
+    updates['cumsum'] = updates['min_since_prev'].cumsum()
+    updates.loc[updates['min_since_prev'] == 0, 'restart'] = -1 * updates['cumsum']
+    updates['restart'] = updates['restart'].fillna(0).cumsum()
+    updates['cumsum'] = updates['cumsum'] + updates['restart']
+    updated_stop_times = pd.DataFrame()
+    for project in updates.project_id.unique():
+        updates_project = updates[updates['project_id'] == project]
+        mode = updates_project['mode'].unique()[0]
+        original_path = ('data/processed/gtfs_baseline/%s' % mode)
+        trips = pd.read_csv('%s/trips.txt' % original_path)
+        stop_times = pd.read_csv('%s/stop_times.txt' % original_path)
+        stop_times_routes = stop_times.merge(trips[['trip_id', 'route_id', 'service_id']], on='trip_id', how='left')
+        for route in updates_project['route_id'].unique():
+            updates_project_route = updates_project[updates_project['route_id'] == route]
+            updates_project_route['prev_stop_id'] = updates_project_route['stop_id'].shift().fillna(0).astype('int')
+            updates_project_route['to_from'] = updates_project_route['prev_stop_id'].astype('str') + '_' + updates_project_route['stop_id'].astype('str')
+            stop_times_route = stop_times_routes[stop_times_routes['route_id'] == route]
+            stop_times_route = stop_times_route.sort_values(by=['trip_id', 'service_id', 'stop_sequence'])
+            stop_times_route = time_to_seconds(stop_times_route, ['arrival_time', 'departure_time'])
+            arrival_min = stop_times_route.groupby('trip_id')['arrival_time'].min()
+            departure_min = stop_times_route.groupby('trip_id')['departure_time'].min()
+            stop_times_route = stop_times_route.set_index('trip_id')
+            stop_times_route['arrival_min'] = arrival_min
+            stop_times_route['departure_min'] = departure_min
+            stop_times_route['prev_stop_id'] = stop_times_route['stop_id'].shift().fillna(0).astype('int')
+            stop_times_route['to_from'] = stop_times_route['prev_stop_id'].astype('str') + '_' + stop_times_route['stop_id'].astype('str')
+            stop_times_route = stop_times_route.reset_index()
+            stop_times_route = stop_times_route.merge(updates_project_route[['to_from', 'cumsum']], on=['to_from'], how='left')
+            stop_times_route['arrival_time'] = stop_times_route['arrival_min'] + stop_times_route['cumsum'].fillna(0) * 60
+            stop_times_route['departure_time'] = stop_times_route['departure_min'] + stop_times_route['cumsum'].fillna(0) * 60
+            stop_times_route['arrival_time'] = pd.to_datetime(stop_times_route['arrival_time'].round(), unit='s').dt.time
+            stop_times_route['departure_time'] = pd.to_datetime(stop_times_route['departure_time'].round(), unit='s').dt.time
+            stop_times_route = stop_times_route[list(stop_times.columns)]
+            updated_stop_times = updated_stop_times.append(stop_times_route)
+    equal_stop_times = stop_times_routes[~stop_times_routes['route_id'].isin(updates['route_id'])][list(stop_times.columns)]
+    updated_stop_times.append(equal_stop_times).to_csv('data/processed/gtfs_project/%s/stop_times.txt' % mode)
+
+
 def run(start_time, end_time, weekday):
-    create_ua_network(start_time, end_time, weekday)
-    net = create_pandana_network()
-    net, zones = read_process_zones(net)
-    calculate_indicators(net, zones)
+    for scenario in ['baseline', 'project']:
+        create_ua_network(scenario, start_time, end_time, weekday)
+        net = create_pandana_network()
+        net, zones = read_process_zones(net)
+        calculate_indicators(scenario, net, zones)
+    compare_indicators(zones)
 
 
-def create_ua_network(start_time, end_time, weekday):
+def create_ua_network(scenario, start_time, end_time, weekday):
     print('Creating UrbanAccess Network')
-    gtfs_path = './data/processed_gtfs'
-    bbox = (-60.8755, -36.2946, -57.0025,-33.3997)
+    gtfs_path = './data/processed/gtfs_%s' % scenario
+    bbox = (-60.8755, -36.2946, -57.0025, -33.3997)
+    bbox = (-58.4747, -34.6404, -58.4143, -34.5997)  # CABALLITO
+    nodes, edges = ua.osm.load.ua_network_from_bbox(bbox=bbox, remove_lcn=True)
+    ua.osm.network.create_osm_net(osm_edges=edges, osm_nodes=nodes, travel_speed_mph=3)
     loaded_feeds = ua.gtfs.load.gtfsfeed_to_df(gtfsfeed_path=gtfs_path, bbox=bbox, validation=True,
                                                verbose=True, remove_stops_outsidebbox=True,
                                                append_definitions=True)
-    nodes, edges = ua.osm.load.ua_network_from_bbox(bbox=bbox, remove_lcn=True)
-    ua.osm.network.create_osm_net(osm_edges=edges, osm_nodes=nodes, travel_speed_mph=3)
     ua.gtfs.network.create_transit_net(gtfsfeeds_dfs=loaded_feeds,
                                        day=weekday,
                                        timerange=[start_time, end_time],
@@ -197,13 +270,13 @@ def create_ua_network(start_time, end_time, weekday):
                                        simplify=True)
     loaded_feeds = ua.gtfs.headways.headways(loaded_feeds, [start_time, end_time])
     ua.network.integrate_network(urbanaccess_network=ua.network.ua_network, urbanaccess_gtfsfeeds_df=loaded_feeds, headways=True)
-    ua.network.save_network(urbanaccess_network=ua.network.ua_network, filename='final_net.h5', overwrite_key=True)
+    ua.network.save_network(urbanaccess_network=ua.network.ua_network, filename='final_%s_net.h5' % scenario, overwrite_key=True)
 
 
-def create_pandana_network():
+def create_pandana_network(scenario):
     print('Loading Precomputed UrbanAccess Network')
-    ua_net = ua.network.load_network(filename='final_net.h5')
-    #export_shp(ua_net.net_nodes, ua_net.net_edges)
+    ua_net = ua.network.load_network(filename='final_%s_net.h5' % scenario)
+    export_shp(ua_net.net_nodes, ua_net.net_edges)
 
     print('Creating Pandana Network')
     s_time = time.time()
@@ -214,11 +287,6 @@ def create_pandana_network():
                        ua_net.net_edges[["weight"]],
                        twoway=False)
     print('Took {:,.2f} seconds'.format(time.time() - s_time))
-    #precompute_time = 45
-    #print('Precomputing network for distance %s.' % precompute_time)
-    #print('Network precompute starting.')
-    #net.precompute(precompute_time)
-    #print('Network precompute done.')
     return net
 
 
@@ -242,40 +310,59 @@ def export_shp(nodes, edges, name_shp='test', df=None):
 
 
 def read_process_zones(net):
-    zones = gpd.read_file('data/original_jobs/Empleo.shp')
+    zones = gpd.read_file('data/jobs_processed/jobs_hexagons.shp')
+    zones['centroid'] = zones['geometry'].centroid
+    zones = zones.set_geometry('centroid')
+    zones = zones.to_crs(4326)
     zones['x'] = zones['geometry'].centroid.x
     zones['y'] = zones['geometry'].centroid.y
     zones['node_id'] = net.get_node_ids(zones['x'], zones['y'])
-    net.set(zones.node_id, variable=zones.jobtotal, name='jobs')
+    zones = zones.to_crs(22192)
+    zones = zones.set_geometry('geometry').drop(columns='centroid')
+    net.set(zones.node_id, variable=zones.jobs, name='jobs')
     zones = zones.set_index('node_id')
     return net, zones
 
 
-def calculate_indicators(net, zones):
+def calculate_indicators(scenario, net, zones):
     s_time = time.time()
     print('Aggregating variables')
     for i in [15, 30, 45]:
         zones['jobs_' + str(i)] = net.aggregate(i, type='sum', decay='linear', name='jobs')
     print('Took {:,.2f} seconds'.format(time.time() - s_time))
-    zones.plot('jobs_45', cmap='gist_heat_r', edgecolor='none', figsize=(20,20), legend=True)
-    zones.plot('jobtotal', cmap='gist_heat_r', edgecolor='none', figsize=(20,20), legend=True)
-    zones.to_file('results.shp')
+    #zones.plot('jobs_45', cmap='gist_heat_r', edgecolor='none', figsize=(20,20), legend=True)
+    #zones.plot('jobtotal', cmap='gist_heat_r', edgecolor='none', figsize=(20,20), legend=True)
+    if not os.path.exists('results'):
+        os.makedirs('./results')
+    zones['jobs_15', 'jobs_30', 'jobs_45'].to_csv('results/%s.csv' % scenario)
+
+
+def compare_indicators(zones):
+    baseline = pd.read_csv('results/baseline.csv')
+    project = pd.read_csv('results/project.csv')
+    breakpoint()
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--update_gtfs", action="store_true", default=False, help="update_gtfs")
+    parser.add_argument("-uj", "--update_jobs", action="store_true", default=False, help="update_jobs")
+    parser.add_argument("-ug", "--update_gtfs", action="store_true", default=False, help="update_gtfs")
     parser.add_argument("-st", "--start_time", type=str, default=False, help="start time for_analysis in 24 hr format (ej 07:00)")
     parser.add_argument("-et", "--end_time", type=str, default=False, help="end time for analysis in 24 hr format (ej 08:00)")
     parser.add_argument("-d", "--weekday", type=str, default=False, help="week day for analysis in 24 hr format (ej monday)")
     args = parser.parse_args()
+
+    update_jobs = args.update_jobs if args.update_jobs else False
     update_gtfs = args.update_gtfs if args.update_gtfs else False
     start_time = args.start_time if args.start_time else '07:00:00'
     end_time = args.end_time if args.end_time else '08:00:00'
     weekday = args.weekday if args.weekday else 'monday'
 
+    if update_jobs:
+        process_update_jobs()
     if update_gtfs:
-        process_gtfs()
+        process_update_gtfs()
     run(start_time, end_time, weekday)
 
 
